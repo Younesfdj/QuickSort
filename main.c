461b5717a62c6af85331a7d39cec6f928c7e1398
Hichem-Mabed <mabedhichem28@gmail.com>
2023-01-02 01:25:54 +0100

    fonction Ta3 Modifier Article

--- main.c
+++ main.c
@@ -15,6 +15,7 @@ struct Article
     int QTC; // Quantity
     char *Designation;
     int Prix;
+    int Critique; // si 1 qlors oui sinon 0 alors non
     Date date_per;
     Article *suiv;
 };
@@ -48,6 +49,7 @@ Aliste *initAliste() /// -- MARCHE COMME INITPILE --
     new->date_per.aaaa = 0;
     new->date_per.jj = 0;
     new->date_per.mm = 0;
+    new->Critique = 0;
     new->suiv = NULL;
     Pile->Sommet = new;
     return Pile;
@@ -62,11 +64,12 @@ void AjouArti(Aliste *Pile, Article NewArt) /// NewArt = nouveau Article a ajout
     new->date_per.aaaa = NewArt.date_per.aaaa;
     new->date_per.mm = NewArt.date_per.mm;
     new->date_per.jj = NewArt.date_per.jj;
+    new->Critique = NewArt.Critique;
     new->suiv = Pile->Sommet;
     Pile->Sommet = new;
 }
 
-Article SuppArti(Aliste *Pile) /// --- CA MARCHE COMME DEPILER ---
+Article DepilerArticle(Aliste *Pile) /// --- CA MARCHE COMME DEPILER et elle renvoi un article ---
 {
     Article *new = malloc(sizeof(*new));
     new->Designation = Pile->Sommet->Designation;
@@ -75,31 +78,96 @@ Article SuppArti(Aliste *Pile) /// --- CA MARCHE COMME DEPILER ---
     new->date_per.aaaa = Pile->Sommet->date_per.aaaa;
     new->date_per.jj = Pile->Sommet->date_per.jj;
     new->date_per.mm = Pile->Sommet->date_per.mm;
+    new->Critique = Pile->Sommet->Critique;
     new->suiv = NULL;
     Pile->Sommet = Pile->Sommet->suiv;
     return *new;
 }
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//-------------------------les fonctions D'affichage---------------------------------------------------------//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 /// AFFICHAGE CE FAIT COMME UNE LISTE !!
 void affArtiList(Aliste *Pile)
 {
-    Article *temp = Pile->Sommet;
-    while (temp->suiv != NULL)
+    Aliste *TempPile = initAliste();
+    Article temp;
+
+    while (Pile->Sommet->suiv != NULL) // affichage
     {
-        printf("\n%s\tQTC:%d\tPrix:%dda ", temp->Designation, temp->QTC, temp->Prix);
-        if (temp->date_per.aaaa != 0)
-            printf("Date %d/%d/%d", temp->date_per.jj, temp->date_per.mm, temp->date_per.aaaa);
-        temp = temp->suiv;
+        temp = DepilerArticle(Pile);
+        if (temp.Critique == 1)
+        {
+            printf("\n%s\tQTC:%d\tPrix:%dda Critique Date %d/%d/%d", temp.Designation, temp.QTC, temp.Prix, temp.date_per.jj, temp.date_per.mm, temp.date_per.aaaa);
+        }
+        else if (temp.Critique == 0)
+        {
+            printf("\n%s\tQTC:%d\tPrix:%dda Non-Critique Date %d/%d/%d", temp.Designation, temp.QTC, temp.Prix, temp.date_per.jj, temp.date_per.mm, temp.date_per.aaaa);
+        }
+        AjouArti(TempPile, temp);
+    }
+    while (TempPile->Sommet->suiv != NULL)
+    {
+        temp = DepilerArticle(TempPile);
+        AjouArti(Pile, temp);
     }
 }
-/// --- SUPPRIMER UN ARTICLE DEFINIE PAR DESIGNIATION ---
-void SuppArti2(Aliste *Pile, Article Art_a_supp)
+
+// AFFICHAGE DES ARTICLES CRITIQUES
+void AffArtCrit(Aliste *Pile)
+{
+    Aliste *TempPile = initAliste();
+    Article temp;
+
+    while (Pile->Sommet->suiv != NULL) // affichage
+    {
+        temp = DepilerArticle(Pile);
+        if (temp.Critique == 1)
+        {
+            printf("\n%s\tQTC:%d\tPrix:%dda  Date %d/%d/%d", temp.Designation, temp.QTC, temp.Prix, temp.date_per.jj, temp.date_per.mm, temp.date_per.aaaa);
+        }
+        AjouArti(TempPile, temp);
+    }
+    while (TempPile->Sommet->suiv != NULL)
+    {
+        temp = DepilerArticle(TempPile);
+        AjouArti(Pile, temp);
+    }
+}
+
+// AFFICHAGE DES ARTICLES NON CRITIQUES
+void AffArtNonCrit(Aliste *Pile)
+{
+    Aliste *TempPile = initAliste();
+    Article temp;
+
+    while (Pile->Sommet->suiv != NULL) // affichage
+    {
+        temp = DepilerArticle(Pile);
+        if (temp.Critique == 0)
+        {
+            printf("\n%s\tQTC:%d\tPrix:%dda Date %d/%d/%d", temp.Designation, temp.QTC, temp.Prix, temp.date_per.jj, temp.date_per.mm, temp.date_per.aaaa);
+        }
+        AjouArti(TempPile, temp);
+    }
+    while (TempPile->Sommet->suiv != NULL)
+    {
+        temp = DepilerArticle(TempPile);
+        AjouArti(Pile, temp);
+    }
+}
+
+/////////////////////// --- SUPPRIMER UN ARTICLE DEFINIE PAR DESIGNIATION ---//////////////////////////////
+
+void SupprimerArticle(Aliste *Pile, Article Art_a_supp) //----suppression d'un article precis---
 {
     Article x;
     Aliste *Pilei = initAliste();
     int trouve = 0;
     while (Pile->Sommet != NULL && trouve == 0)
     {
-        x = SuppArti(Pile);
+        x = DepilerArticle(Pile);
         if (strcmp(x.Designation, Art_a_supp.Designation) == 0)
         {
             trouve = 1;
@@ -112,12 +180,13 @@ void SuppArti2(Aliste *Pile, Article Art_a_supp)
     // --- PARTIE RECONSTRUCION ---
     while (Pilei->Sommet->suiv != NULL)
     {
-        x = SuppArti(Pilei);
+        x = DepilerArticle(Pilei);
         AjouArti(Pile, x);
     }
 }
 
-/// Rechereche par caractéristiques
+//////////////////------------ Rechereche par caractéristiques-------------////////////
+
 void Rech_carac(Aliste *P, Article Art_a_rech, char option)
 {
     if ((int)option == 49 || (int)option == 50)
@@ -127,7 +196,7 @@ void Rech_carac(Aliste *P, Article Art_a_rech, char option)
         short int exist = 0;
         while (P->Sommet != NULL)
         {
-            x = SuppArti(P);
+            x = DepilerArticle(P);
             AjouArti(I, x);
             switch (option)
             {
@@ -154,7 +223,7 @@ void Rech_carac(Aliste *P, Article Art_a_rech, char option)
         // --- PARTIE RECONSTRUCION ---
         while (I->Sommet->suiv != NULL)
         {
-            x = SuppArti(I);
+            x = DepilerArticle(I);
             AjouArti(P, x);
         }
     }
@@ -207,7 +276,7 @@ void Affiche_arti_exp(Aliste *P)
 
     while (P->Sommet != NULL)
     {
-        x = SuppArti(P);
+        x = DepilerArticle(P);
         AjouArti(I, x);
         if (DateCmp(current_date, x.date_per) == 1 || DateCmp(current_date, x.date_per) == 0)
             printf("%s\tQTC:%d\tPrix:%d da Date:%d/%d/%d\n", x.Designation, x.QTC, x.Prix, x.date_per.jj, x.date_per.mm, x.date_per.aaaa);
@@ -215,8 +284,101 @@ void Affiche_arti_exp(Aliste *P)
     // --- PARTIE RECONSTRUCION ---
     while (I->Sommet->suiv != NULL)
     {
-        x = SuppArti(I);
+        x = DepilerArticle(I);
         AjouArti(P, x);
+    }
+}
+
+//////////////////////---------------- MODIFICATION ARTICLE -------------------/////////////
+void ModifierArt(Aliste *Pile, char *referance)
+{
+    Aliste *TempP = initAliste();
+    Article x;
+    int R, n, NB;   // R pour test du boocle DO WHILE / n pout le test du switch / NB variable intermediaire pour les chengement entier
+    char *ND;       // ND variable intermediaire pour changement des chaines
+    int NJ, NM, NA; // variables intermediaires pour changement du date
+    int trouve;
+
+    trouve = 0;
+    while (Pile->Sommet->suiv != NULL && trouve == 0)
+    {
+        x = DepilerArticle(Pile);
+        if (strcmp(x.Designation, referance) == 0)
+        {
+            trouve = 1;
+        }
+        else
+        {
+            AjouArti(TempP, x);
+        }
+    }
+    do
+    {
+        printf("\nQue voulez vous modifier ?(choisissez par numero)\n");
+        printf("1-Quantite\n");
+        printf("2-Prix\n");
+        printf("3-Designation\n");
+        printf("4-Date d'expiration\n");
+        printf("5-Critique ou non\n");
+        scanf("%d", &n);
+        switch (n)
+        {
+        case 1:
+            printf("donner la nouvelle quantite: \n");
+            scanf("%d", &NB);
+            x.QTC = NB;
+            break;
+
+        case 2:
+            printf("donner le nouveau Prix:");
+            scanf("%d", &NB);
+            x.Prix = NB;
+            break;
+
+        case 3:
+            printf("donner la nouvelle Designation: ");
+            scanf("%s", &ND);
+            strcpy(x.Designation, ND);
+            break;
+
+        case 4:
+            printf("donner la nouvelle Date d'expiration:");
+            scanf("%d", &NJ);
+            scanf("%d", &NM);
+            scanf("%d", &NA);
+            x.date_per.jj = NJ;
+            x.date_per.mm = NM;
+            x.date_per.aaaa = NA;
+            break;
+
+        case 5:
+            printf("Critique ou Non Critique(oui/non):");
+            scanf("%s", &ND);
+            printf("test");
+            int EE = strcmp(ND, "oui");
+            printf("%d", EE);
+            if (EE == 0)
+            {
+                x.Critique = 1;
+            }
+            else
+            {
+                x.Critique = 0;
+            }
+            break;
+
+        default:
+            printf("SVP choisir un numero de 1 a 5 !");
+            break;
+        }
+        printf("\n voulez vous faire un autre changement ?(Si oui tapez 1)");
+        scanf("%d", &R);
+    } while (R == 1);
+    AjouArti(Pile, x);
+    while (TempP->Sommet->suiv != NULL)
+    {
+        x = DepilerArticle(TempP);
+        AjouArti(Pile, x);
     }
 }
 
@@ -228,16 +390,19 @@ void main()
     produit1.Designation = "iPhoneXS";
     produit1.Prix = 130;
     produit1.QTC = 20;
+    produit1.Critique = 0;
 
     Article produit2;
     produit2.Designation = "SamsungA10";
     produit2.Prix = 10;
     produit2.QTC = 10;
+    produit2.Critique = 1;
 
     Article produit3;
     produit3.Designation = "Redmi7i";
     produit3.Prix = 25;
     produit3.QTC = 44;
+    produit3.Critique = 1;
 
     produit3.date_per.jj = 12;
     produit3.date_per.mm = 12;
@@ -255,6 +420,7 @@ void main()
     produit4.Designation = "Redmi7ii";
     produit4.Prix = 25;
     produit4.QTC = 44;
+    produit4.Critique = 0;
 
     produit4.date_per.jj = 0;
     produit4.date_per.mm = 0;
@@ -290,6 +456,14 @@ void main()
     Affiche_arti_exp(AA);
 
     affArtiList(AA);
+    printf("\n");
+    AffArtCrit(AA);
+    printf("\n");
+    affArtiList(AA);
+    printf("\n");
+    AffArtNonCrit(AA);
+    char *tt;
+    tt = "iPhoneXS";
+    ModifierArt(AA, tt);
+    affArtiList(AA);
 }
-
-// ya youneeeessssssssss
